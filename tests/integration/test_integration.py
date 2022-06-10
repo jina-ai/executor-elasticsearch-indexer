df8c93a88ccfa69581179a262fc9aefba7f4fa5b
Alaeddine Abdessalem <alaeddine-13@live.fr>
2022-06-10 14:43:02 +0200

    test: cover filtering

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,3 +1,5 @@
+import operator
+
 import numpy as np
 import pytest
 from docarray import Document, DocumentArray
@@ -41,3 +43,54 @@ def test_reload_keep_state(docker_compose):
     with f:
         second_search = f.search(inputs=docs)
         assert len(first_search[0].matches) == len(second_search[0].matches)
+
+numeric_operators_elasticsearch = {
+    'gte': operator.ge,
+    'gt': operator.gt,
+    'lte': operator.le,
+    'lt': operator.lt,
+    'eq': operator.eq,
+}
+
+
+@pytest.mark.parametrize('operator', list(numeric_operators_elasticsearch.keys()))
+def test_filtering(docker_compose, operator: str):
+    n_dim = 2
+
+    f = Flow().add(
+        uses=ElasticSearchIndexer,
+        uses_with={
+            'n_dim': n_dim,
+            'columns': [('price', 'float')],
+        },
+    )
+
+    docs = DocumentArray(
+        [
+            Document(id=f'r{i}', embedding=np.random.rand(n_dim), tags={'price': i})
+            for i in range(50)
+        ]
+    )
+    with f:
+
+        f.index(docs)
+
+        for threshold in [10, 20, 30]:
+            filter_ = {
+                'range': {
+                    'price': {
+                        operator: threshold,
+                    }
+                }
+            }
+            doc_query = DocumentArray([Document(embedding=np.random.rand(n_dim))])
+            indexed_docs = f.search(doc_query, parameters={'filter': filter_})
+
+            assert len(indexed_docs[0].matches) > 0
+
+            assert all(
+                [
+                    numeric_operators_elasticsearch[operator](r.tags['price'], threshold)
+                    for r in indexed_docs[0].matches
+                ]
+            )
