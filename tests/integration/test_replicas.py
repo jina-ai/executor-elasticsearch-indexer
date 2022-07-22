015b4dc9732441a8b6c78f232f14a6eac3f2b9f9
winstonww <winstonwongww2@gmail.com>
2022-07-22 17:06:17 +0800

    fix: address comments

--- tests/integration/test_replicas.py
+++ tests/integration/test_replicas.py
@@ -7,37 +7,34 @@ from executor import ElasticSearchIndexer
 
 def test_replicas(docker_compose):
     n_dim = 10
-    
+
     f = Flow().add(
         uses=ElasticSearchIndexer,
         uses_with={'index_name': 'test1', 'n_dim': n_dim},
     )
-    
+
     docs_index = [Document(embedding=np.random.random(n_dim)) for _ in range(1000)]
-    
+
     docs_query = docs_index[:100]
-    
+
     with f:
-        f.post(
-            on='/index',
-            inputs=docs_index,
+        f.post(on='/index', inputs=docs_index, request_size=1)
+
+        docs_without_replicas = sorted(
+            f.post(on='/search', inputs=docs_query, request_size=1),
+            key=lambda doc: doc.id,
         )
-    
-        docs_without_replicas = f.post(
-            on='/search',
-            inputs=docs_query,
-        )
-    
+
     f_with_replicas = Flow().add(
         uses=ElasticSearchIndexer,
         uses_with={'index_name': 'test1', 'n_dim': n_dim},
-        replicas=4
+        replicas=4,
     )
-    
-    with f_with_replicas :
-        docs_with_replicas = f_with_replicas.post(
-            on='/search',
-            inputs=docs_query,
+
+    with f_with_replicas:
+        docs_with_replicas = sorted(
+            f_with_replicas.post(on='/search', inputs=docs_query, request_size=1),
+            key=lambda doc: doc.id,
         )
-    
+
     assert docs_without_replicas == docs_with_replicas
